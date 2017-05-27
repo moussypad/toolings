54b6889965611b0523d95e5fe0fbcf0bf0a5f235
goldenbearkin <huihangkin@gmail.com>
2017-05-28 05:19:25 +0800

    modify test-script.sh

--- test-script.sh
+++ test-script.sh
@@ -2,7 +2,8 @@
 
 # To exit the script as soon as one of the following commands failed
 set -e 
-for f in `pwd`/**/*/*.yaml
+# for f in `pwd`/**/*/*.yaml
+for f in $(find `pwd` -type f -name '*.yaml')
 do
   echo $f
   aws cloudformation validate-template --template-body file://$f
